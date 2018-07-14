58e73e8085e65dbae36811c54635be4ee6a47d79
vvsch <vvsch@LAPTOP-BN080M66>
2018-07-14 16:44:28 -0700

    MYTESTCLASSS FILE ADDED

---
+++ src/MyTest/MyTestClass.java
@@ -0,0 +1,22 @@
+package MyTest;
+import java.lang.*;
+import java.util.*;
+import java.sql.*;
+import java.io.*;
+import java.lang.*;
+
+
+public class MyTestClass{	
+	static String Name1;
+	static String Name2;
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		Name1 ="MY NAME IS";
+		Name2="AND WHAT IS YOUR NAME?";
+		System.out.println(Name1);
+	}
+}
+
+
---
+++ src/MyTest/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author vvsch
+ *
+ */
+package MyTest;
